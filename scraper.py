3eba485a6f92abbbe9ef9919515e43fcb52621a3
MarSavar <Marbbk123x*>
2020-09-17 19:36:28 +0200

    Cleaner code

--- scraper.py
+++ scraper.py
@@ -12,12 +12,18 @@ suggestions = dict()
 for elem in elements:
     try:
         full_suggestion = elem.get_attribute("textContent").split(":")
-        artist, piece = full_suggestion[0], full_suggestion[1]
-        suggestions[artist] = ["piece",piece.strip().lstrip("“").rstrip("”")]
-    except:
-        suggestions[artist] = ["album", artist]
+        artist, piece = full_suggestion[0], full_suggestion[1].strip().lstrip("“").rstrip("”")
+        suggestion_type = "piece"
+    except IndexError:
+        suggestion_type = "album"
+        artist = piece = elem.get_attribute("textContent")
 
-for k,v in suggestions.items():
-    print(k,v)
+    if artist not in suggestions.keys():
+        suggestions[artist] = [(suggestion_type, piece)]
+    else:
+        suggestions[artist].append((suggestion_type, artist))
+
+for k, v in suggestions.items():
+    print(k," ->", v)
 
 driver.close()
