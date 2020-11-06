9c1c8eff02b26b871f950a84008dec120ed5cb5d
msavar03 <msavar03@dcs.bbk.ac.uk>
2020-11-06 19:42:41 +0100

    Improved API query performance.

--- src/queryAPI.py
+++ src/queryAPI.py
@@ -29,41 +29,43 @@ def query_spotify_api():
     # Update JSON file
     for k, v in sugg.items():
         for i in range(len(v)):
-            data = urlencode({"q": f"album:{v[i]['album']} artist:{k}", "type": "album"})
-            lookup_url = f"{endpoint}?{data}"
-            r = requests.get(lookup_url, headers=headers)
-            y = json.loads(r.text)
-
-            try:
-                # Spotify album ID
-                sugg[k][i]['id'] = y['albums']['items'][0]['uri'].split(":")[2]
-
-            except IndexError as e:
-                data = urlencode({"q": f"album:{v[i]['album']}", "type": "album"})
+            # Don't query Spotify if data already present in the JSON file
+            if 'id' not in sugg[k][i].keys():
+                data = urlencode({"q": f"album:{v[i]['album']} artist:{k}", "type": "album"})
                 lookup_url = f"{endpoint}?{data}"
                 r = requests.get(lookup_url, headers=headers)
                 y = json.loads(r.text)
+
                 try:
                     # Spotify album ID
                     sugg[k][i]['id'] = y['albums']['items'][0]['uri'].split(":")[2]
+
                 except IndexError as e:
+                    data = urlencode({"q": f"album:{v[i]['album']}", "type": "album"})
+                    lookup_url = f"{endpoint}?{data}"
+                    r = requests.get(lookup_url, headers=headers)
+                    y = json.loads(r.text)
+                    try:
+                        # Spotify album ID
+                        sugg[k][i]['id'] = y['albums']['items'][0]['uri'].split(":")[2]
+                    except IndexError as e:
+                        pass
+
+                try:
+                    artist_name = y['albums']['items'][0]['artists'][0]['name']
+
+                    # Check that the Spotify artist matches the New Yorker suggestion
+                    if artist_name.lower() in k.lower():
+                        sugg[k][i]['artist'] = k
+                    else:
+                        sugg[k][i]['artist'] = None
+                except:
                     pass
-
-            try:
-                artist_name = y['albums']['items'][0]['artists'][0]['name']
-
-                # Check that the Spotify artist matches the New Yorker suggestion
-                if artist_name in k:
-                    sugg[k][i]['artist'] = k
-                else:
-                    sugg[k][i]['artist'] = None
-            except:
-                pass
-            try:
-                # Spotify 300x300 album cover URL
-                sugg[k][i]['art'] = y['albums']['items'][0]['images'][1]['url'].split("/")[-1]
-            except:
-                pass
+                try:
+                    # Spotify 300x300 album cover URL
+                    sugg[k][i]['art'] = y['albums']['items'][0]['images'][1]['url'].split("/")[-1]
+                except:
+                    pass
 
     # Update JSON file and upload it to the S3 bucket
     content_object.put(Body=bytes(json.dumps(sugg).encode('UTF-8')), ACL='public-read')
