eecd643575967682c702d21489160a532c0b095c
msavar03 <msavar03@dcs.bbk.ac.uk>
2020-12-07 01:10:06 +0100

    Tests.

---
+++ src/tests.py
@@ -0,0 +1,24 @@
+import pytest
+from credentials import *
+from spotifyAPI import *
+
+spotify = SpotifyAPI(client_id, client_secret)
+
+
+def test_Spotify_API_auth():
+    assert spotify.perform_auth() == True
+
+
+def test_Spotify_API_auth2():
+    assert type(spotify.get_client_credentials()) is not None
+
+
+def test_Spotify_API_auth3():
+    assert type(spotify.get_client_credentials()) == str
+
+
+def test_Spotify_API_auth4():
+    decoded_creds = spotify.get_client_credentials()
+    encode_creds = base64.b64decode(decoded_creds)
+    encode_decode = base64.b64encode(encode_creds).decode()
+    assert decoded_creds == encode_decode
