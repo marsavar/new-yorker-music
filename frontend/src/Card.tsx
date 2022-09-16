79b0430e7d8835874c54da8f3bc3e96dc3a10ead
Mario Savarese <mario.savarese@theguardian.com>
2022-09-16 18:31:33 +0100

    Use CSS animation, remove JS

--- frontend/src/Card.tsx
+++ frontend/src/Card.tsx
@@ -1,38 +1,32 @@
-import { FC, useState } from "react";
-import { useSpring, a } from "@react-spring/web";
+import { CSSProperties, FC, useState } from "react";
 import { ValidAlbum } from "./App";
 import svgLoader from "./loader.svg";
 
-const dedupAlbum = (album: string, artist: string) => {
+const dedupAlbum = (album: string, artist: string): string => {
   if (album === artist) {
     return album;
   }
   return `${album} (${artist})`;
 };
 
+const isLoadingStyle = (isLoading: boolean): CSSProperties => {
+  if (isLoading) {
+    return {
+      background: `url('${svgLoader}') center center no-repeat`,
+    };
+  }
+  return { background: "none" };
+};
+
 export const Card: FC<{ album: ValidAlbum }> = ({ album }) => {
   const [isLoading, setIsLoading] = useState(true);
-  const [flipped, setFlipped] = useState(false);
-  const { transform, opacity } = useSpring({
-    opacity: flipped ? 1 : 0,
-    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
-    config: { mass: 5, tension: 150, friction: 80 },
-  });
+
   const description = dedupAlbum(album.album, album.artist);
 
   return (
-    <span
-      className="cardcont"
-      onMouseEnter={() => setFlipped((state) => !state)}
-      onMouseLeave={() => setFlipped((state) => !state)}
-    >
-      <div>
-        <a.div
-          style={{
-            transform,
-            display: flipped ? "none" : "",
-          }}
-        >
+    <span className="cardcont">
+      <div className="card">
+        <div className="card__face card__face--front">
           <img
             loading="lazy"
             src={`https://i.scdn.co/image/${album.art}`}
@@ -40,29 +34,22 @@ export const Card: FC<{ album: ValidAlbum }> = ({ album }) => {
           />
           <p className="withincard">{album.artist}</p>
           <p className="withincard_sub">{album.album}</p>
-        </a.div>
-        <a.div
-          style={{
-            transform,
-            rotateY: "180deg",
-            display: !flipped ? "none" : "",
-            background: isLoading
-              ? `url('${svgLoader}') center center no-repeat`
-              : "none",
-          }}
-        >
-          <iframe
-            onLoad={() => setIsLoading(false)}
-            loading="lazy"
-            src={`https://open.spotify.com/embed/album/${album.id}`}
-            width="100%"
-            height="380px"
-            frameBorder="0"
-            style={{ backgroundColor: "transparent" }}
-            allow="encrypted-media"
-            title={description}
-          />
-        </a.div>
+        </div>
+        <div className="card__face card__face--back">
+          <div style={isLoadingStyle(isLoading)}>
+            <iframe
+              onLoad={() => setIsLoading(false)}
+              loading="lazy"
+              src={`https://open.spotify.com/embed/album/${album.id}`}
+              width="100%"
+              height="380px"
+              frameBorder="0"
+              style={{ backgroundColor: "transparent" }}
+              allow="encrypted-media"
+              title={description}
+            />
+          </div>
+        </div>
       </div>
     </span>
   );
