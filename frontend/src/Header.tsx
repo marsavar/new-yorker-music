48cfec4d83f52d8516fd79a4bf5b0d34488c3e76
Mario Savarese <mario.savarese@theguardian.com>
2022-09-16 20:05:10 +0100

    Link to NY website

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -3,8 +3,15 @@ export const Header = () => {
     <header>
       <h1>The New Yorker Music Project &#127911;</h1>
       <p>
-        An archive of the New Yorker's Jazz, Rock, and Pop music
-        recommendations scraped by a Lambda function that has been running on AWS since September 2020.
+        An archive of the{" "}
+        <a
+          rel="external noreferrer"
+          href="https://www.newyorker.com/goings-on-about-town/night-life"
+        >
+          New Yorker's Jazz, Rock, and Pop music recommendations
+        </a>{" "}
+        scraped by a Lambda function that has been running on AWS since
+        September 2020.
         <br /> Hover over the covers to listen to each album's tracks.
       </p>
     </header>
